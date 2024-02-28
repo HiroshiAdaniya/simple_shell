b2e8443685efdf62184e64d5b151e7638c0413d8
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-02-28 22:21:25 +0200

    updated: shell.c

--- shell.c
+++ shell.c
@@ -1,41 +1,44 @@
 #include "shell.h"
 /**
- * main - prints 4 environment variables, PID and PPID
+ * simple_shell - a simple shell program
  * Return: 0 on success
  */
 int main(void)
 {
-	int i, j, len = 0;
-	char *list[] = {"USER", "SHELL", "BROWSER", "PATH", NULL};
+	ssize_t child, i = 0;
+	size_t len = 0;
+	char delim[] = " \n", **cmd, *str, *getcmd = NULL;
 
-	for (i = 0; i < 50;  i++)
-		printf("=");
-	printf("\n\n");
-	for (i = 0; list[i] != NULL; i++)
+	cmd = malloc(sizeof(char *) * 2);
+	while (1 && i != EOF)
 	{
-		len = strlen(list[i]);
-		for (j = 0; environ[j] != NULL; j++)
+		printf("($): ");
+		i = 0;
+		i = getline(&getcmd, &len, stdin);
+		if (i == -1)
 		{
-			if (strncmp(list[i], environ[j], len) == 0)
-			{
-				printf("%s: %s\n", list[i], environ[j]);
-				break;
-			}
+			printf("terminated\n");
+			exit(EXIT_FAILURE);
 		}
+		cmd[0] = strtok(getcmd, delim);
+		str = strtok(NULL, delim);
+		if (str != NULL)
+		{
+			cmd[0] = "null";
+			cmd[1] = NULL;
+		}
+		child = fork();
+		if (child == -1)
+			return (-1);
+		if (child == 0)
+		{
+			i = execve(cmd[0], cmd, environ);
+			if (i == -1)
+				perror("failed");
+		}
+		else
+			wait(NULL);
 	}
-	putchar('\n');
-	for (i = 0; i < 50; i++)
-		printf("=");
-	printf("\n\nPID of current proccess:%d\n", getpid());
-	printf("PID of current parent process: %d\n\n", getppid());
-	for (i = 0; i < 50; i++)
-		printf("=");
-	putchar('\n');
-	i = simple_shell();
-	if (i == -1)
-	{
-		printf("Could not run shell\n");
-		exit(EXIT_FAILURE);
-	}
+	free(cmd);
 	return (0);
 }
