e5394a00592488c3ecc8896bdd0b88a2fcb4c466
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-02-28 22:02:57 +0200

    added: shell.h

---
+++ shell.h
@@ -0,0 +1,17 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+extern char **environ;
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <errno.h>
+
+int main(void);
+int simple_shell(void);
+
+#endif
