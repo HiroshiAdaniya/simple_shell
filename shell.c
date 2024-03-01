87c6a4912af694d9c6e6651dba7ccd8bbd510666
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-03-01 22:18:06 +0200

    shell.c

--- shell.c
+++ shell.c
@@ -21,21 +21,39 @@ int main(int argc, char *argv[])
 	}
 	while (1 && i != EOF)
 	{
-		prompt();
 		i = getline(&getcmd, &len, stdin);
 		if (i == -1)
 			free_mem(cmd, getcmd);
 		cmd[0] = strtok(getcmd, delim);
 		cmd[1] = NULL;
 		child = fork();
-		if (child == -1)
+		if (child == -1 || cmd[0] == NULL)
 			free_mem(cmd, getcmd);
 		if (child == 0)
-			_execute(cmd, argv, getcmd);
+		{
+			i = execve(cmd[0], cmd, environ);
+			if (i == -1)
+			{
+				cmderror(argv, cmd);
+				free_mem(cmd, getcmd);
+			}
+		}
 		else
 			wait(NULL);
 	}
 	return (0);
+}
+/**
+ * free_mem - frees memory
+ * @s: pointer to a pointer of char
+ * @z: a pointer to a string
+ * Return: NULL;
+ */
+void free_mem(char **s, char *z)
+{
+	free(s);
+	free(z);
+	exit(0);
 }
 /**
  * cmderror - prints error message if command not found
@@ -46,42 +64,4 @@ int main(int argc, char *argv[])
 void cmderror(char **argv, char **cmd)
 {
 	printf("%s: 1: %s: not found\n", argv[0], cmd[0]);
-}
-/**
- * prompt - displays a prompt
- * Return: nothing / void
- */
-void prompt(void)
-{
-	write(STDOUT_FILENO, "#: ", 3);
-}
-/**
- * _execute - executes a program
- * @cmd: pointer to a pointer of strings
- * @argv: argument vector
- * @getcmd: a pointer to a string
- * Return: nothing / void
- */
-void _execute(char **cmd, char **argv, char *getcmd)
-{
-	ssize_t j = 0;
-
-	j = execve(cmd[0], cmd, environ);
-	if (j == -1)
-	{
-		cmderror(argv, cmd);
-		free_mem(cmd, getcmd);
-	}
-}
-/**
- * free_mem - frees memory
- * @s: pointer to a pointer of char
- * @z: a pointer to a string
- * Return: void
- */
-void free_mem(char **s, char *z)
-{
-	free(s);
-	free(z);
-	exit(0);
 }
