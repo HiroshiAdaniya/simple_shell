439df98c50847149d3f3cd9668b134da28d2848d
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-03-06 17:46:56 +0200

    debugging: shell.c

--- shell.c
+++ shell.c
@@ -10,35 +10,35 @@ int main(int __attribute__((unused))argc, char *argv[])
 	ssize_t i, child = 0;
 	size_t len = 0;
 	char delim[] = " \n";
-	char **getcmd, *string = NULL;
+	char **cmd, *getcmd= NULL;
 
-	getcmd = malloc(sizeof(char *) * 2);
+	cmd = malloc(sizeof(char *) * 2);
 	if (getcmd == NULL)
 	{
-		free(getcmd);
+		free(cmd);
 		return (0);
 	}
 	while (1 && i != EOF)
 	{
 		if (isatty(STDIN_FILENO))
 			write(STDIN_FILENO, "#: ", 3);
-		i = getline(&string, &len, stdin);
+		i = getline(&cmd, &len, stdin);
 		if (i == -1)
 		{
 			write(STDIN_FILENO, "\n", 1);
-			free_mem(getcmd, string);
+			free_mem(cmd, getcmd);
 		}
-		getcmd[0] = strtok(string, delim);
+		cmd[0] = strtok(getcmd, delim);
 		child = fork();
-		if (child == -1 || getcmd[0] == NULL)
-			free_mem(getcmd, string);
+		if (child == -1 || cmd[0] == NULL)
+			free_mem(cmd, getcmd);
 		if (child == 0)
 		{
-			i = execve(getcmd[0], getcmd, environ);
+			i = execve(cmd[0], cmd, environ);
 			if (i == -1)
 			{
 				perror(argv[0]);
-				free_mem(getcmd, string);
+				free_mem(cmd, getcmd);
 			}
 		}
 		else
@@ -58,3 +58,4 @@ void free_mem(char **s, char *z)
 	free(z);
 	exit(0);
 }
+
