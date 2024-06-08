f7404182514e2886227419d0932f054fd26a778d
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-06-08 19:30:50 +0200

    shell.h

--- shell.h
+++ shell.h
@@ -1,18 +1,37 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+/** 
+ * Global variables
+ */
+
 extern char **environ;
+
+/**
+ * Libraries 
+ */
 
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
 #include <errno.h>
 #include <stdbool.h>
+#include <fcntl.h>
 
-void free_mem(char **, char *);
-char *NoNewline(char *);
-void Error(char **, char *);
+/**
+ * Function prototypes
+ */
+
+void prompt(void);
+void print(char *);
+int strlength(char *);
+char *read_line(void);
+void execute_command(char *, char *);
+
+char **allocation(char *);
 #endif
