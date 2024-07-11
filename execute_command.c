cb7bea6b90b95f180b8183230749e91d042b5969
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-07-11 10:58:49 +0200

    debugging

--- execute_command.c
+++ execute_command.c
@@ -21,7 +21,7 @@ int execute_command(char *program, string *terminal, int flag)
 		}
 		terminal->len = path_search(path, terminal);
 	}
-	if (terminal->len == 0)
+	if (terminal->len == 0 && path != NULL)
 		forking(program, terminal);
 	free_terminal_memory(&terminal);
 
