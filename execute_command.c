496de4725e3bdb409c9217375ac2a8c8e77d53b5
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-07-12 15:44:04 +0200

    debugging: execute_command.c

--- execute_command.c
+++ execute_command.c
@@ -10,7 +10,7 @@ int execute_command(char *program, string *terminal, int flag)
 {
 	char *path = NULL;
 	int i = -1;
-	char *real_path = NULL;
+	/*char *real_path = NULL;*/
 
 	path = getenv("PATH");
 	if (path != NULL && strlen(path) != 0)
@@ -24,12 +24,13 @@ int execute_command(char *program, string *terminal, int flag)
 	}
 	else if (path == NULL && terminal->array[0][0] == '/')
 	{
-		real_path = realpath(terminal->array[0], real_path);
+		i = realpath_check(terminal->array[0]);
+		/*real_path = realpath(terminal->array[0], real_path);
 		if (real_path == NULL)
 			i = -1;
 		else
 			i = 0;
-		free(real_path);
+		free(real_path);*/
 	}
 	if (i == 0)
 		forking(program, terminal);
